72a495947da60be5097a9fca2ed5a048d581299c
arman1100 <a1mohammad@ryerson.ca>
2022-04-17 01:45:23 -0400

    1st commit

---
+++ blog/src/components/Home.vue
@@ -0,0 +1,48 @@
+<template>
+<h1> Welcome To Home</h1>
+<h2>Name: {{name}}</h2>
+<h2>Mobile: {{mobile}}</h2>
+<h2>Email: {{getEmail('abc@gmail.com')}}</h2>
+<h3>Date: {{getDate()}}</h3>
+<h4>{{currentDate}}</h4>
+
+</template>
+
+<script>
+
+import { format } from 'date-fns'
+
+export default {
+    name: "Ho-me",
+    data()
+    {
+        return {
+            name: 'Arman',
+            mobile: 9898,
+            getEmail(x)
+            {
+                return x;
+            },
+            currentDate: format(new Date(), 'yyyy-MM-dd HH:mm:ss')
+        }
+    },
+    methods: {
+        getDate()
+        {
+            return 'April 17 2022';
+        }
+    }
+
+
+}
+
+</script>
+
+
+<style scoped>
+
+h1{
+    color: orange;
+}
+
+</style>
---
+++ blog/src/components/WeatherMap.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+     data:  () => {
+        return {
+             weather:""
+        }
+    },
+    mounted() {
+        // fetch on init
+        this.fetchWeather()
+    },
+    methods: {
+    async fetchWeather() {
+      let data = await  (await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Paris,ON,CA&appid=3c2698390e1ec710f4c38eb7d8ccda74&units=metric`)).json();
+      let answer = data.main.temp;
+    this.weather = answer;
+      
+    }
+  }
+}
+</script>
+
+<template>
+    <div>
+      <p>WELCOME TO WEATHER </p>
+        <p> Weather: {{weather}}C</p>
+
+    </div>
+
+</template>
+
+
+
+
+
+<style>
+</style>
